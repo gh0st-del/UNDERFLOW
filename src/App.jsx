013188c4fcae2219a1d717bcc1bd2806ecfe9e5f
auilk <abdelghafour.lbaz01@gmail.com>
2025-05-10 22:33:15 +0100

    Initialize project using Vite and configure Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App()
+{
+  return (
+    <div className="w-full h-dvh flex justify-center items-center">
+      <h1 className="text-6xl font-bold">UNDERFLOW</h1>
+    </div>
+  );
+}
+
+export default App;
