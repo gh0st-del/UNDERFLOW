a050036b5143bc9e450e0a3e77a9c5c7d8d78161
auilk <abdelghafour.lbaz01@gmail.com>
2025-05-25 23:59:02 +0100

    Resize <svg> to align precisely with <text>

--- src/components/TextOutline.jsx
+++ src/components/TextOutline.jsx
@@ -1,13 +1,32 @@
+import { useEffect, useRef } from "react";
+
 function TextOutline({text})
 {
+    const textRef = useRef(null);
+
+    useEffect(() =>
+    {
+        const svg = textRef.current.parentElement;
+        let textBBox = textRef.current.getBBox();
+
+        svg.setAttribute("width", textBBox.width);
+        svg.setAttribute("height", textBBox.height);
+        svg.setAttribute('viewBox', `0 0 ${textBBox.width} ${textBBox.height}`);
+
+        textRef.current.setAttribute('x', -textBBox.x);
+        textRef.current.setAttribute('y', "0");
+        textBBox = textRef.current.getBBox();
+        textRef.current.setAttribute('y', `${-textBBox.y}`);
+    },[text]);
+
     return (
         <svg xmlns="http://www.w3.org/2000/svg">
             <text
+                ref={textRef}
                 fontSize="2rem"
                 stroke="black"
                 strokeWidth={1}
                 fill="none"
-                dominantBaseline="hanging"
             >
                 {text}
             </text>
