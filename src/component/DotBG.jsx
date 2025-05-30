fd68902d9ec993e0a38f2c2a7095df352f7c6688
auilk <abdelghafour.lbaz01@gmail.com>
2025-05-30 20:49:39 +0100

    Initialize DotBG render loop with solid clear color

--- src/component/DotBG.jsx
+++ src/component/DotBG.jsx
@@ -1,8 +1,30 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 function DotBG()
 {
     const canvasRef = useRef(null);
+    const animRequestRef = useRef(null);
+
+    useEffect(() =>
+    {
+        /**@type {WebGL2RenderingContext}*/
+        const gl = canvasRef.current.getContext("webgl2");
+        if (!gl)
+        {
+            console.error("WebGL2 is not supported in this browser.");
+        }
+        const RenderLoop = () =>
+        {
+            gl.clearColor(0.1, 0.1, 0.1, 1.0);
+            gl.clear(gl.COLOR_BUFFER_BIT);
+    
+            animRequestRef.current = requestAnimationFrame(RenderLoop);
+        }
+
+        animRequestRef.current = requestAnimationFrame(RenderLoop);
+
+        return () => cancelAnimationFrame(animRequestRef.current);
+    }, []);
 
     return(
         <canvas
