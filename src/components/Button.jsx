c738f3db3a4c6fb145c963a372a644ac743acf95
auilk <abdelghafour.lbaz01@gmail.com>
2025-06-07 06:10:40 +0100

    Make circle SVG inside Button follow mouse movement

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,29 @@
+import { useLayoutEffect, useRef } from "react";
+
 function Button({label = "button", onClick, hpad = "10px", wpad = "5px", primaryCol = "white", secondaryCol = "black", borderWidth = "2px", radius = "4px", fontWeight = "bold"})
 {
+    const circleRef = useRef(null);
+    const buttonRef = useRef(null);
+
+    useLayoutEffect(() =>
+    {
+        const MouseMoveCallback = (event) =>
+        {
+            const svg = circleRef.current.parentElement;
+            const rect = svg.getBoundingClientRect();
+            const x = event.clientX - rect.left;
+            const y = event.clientY - rect.top;
+            circleRef.current.setAttribute("cx", x);
+            circleRef.current.setAttribute("cy", y);
+        }
+
+        buttonRef.current.addEventListener("mousemove", MouseMoveCallback);
+
+    }, []);
 
     return(
         <button 
+            ref={buttonRef}
             className="font-bold cursor-pointer relative"
             style={{
                 padding: `${wpad} ${hpad}`,
@@ -21,8 +42,7 @@ function Button({label = "button", onClick, hpad = "10px", wpad = "5px", primary
                 xmlns="http://www.w3.org/2000/svg"
             >
                 <circle
-                    cx="0" 
-                    cy="0"
+                    ref={circleRef}
                     r="10" 
                     fill={secondaryCol} />
             </svg>
